3a5b1f35fa6ce2a1d0ce5a51cf43a674b63dda49
GowthamiMurugesan <gowthamimurugesan333@gmail.com>
2016-08-08 10:25:01 +0530

    Add files via upload
    multiply

---
+++ Multiply.java
@@ -0,0 +1,16 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+		int n;
+n=in.nextInt();
+for(int i=1;i<=16;i++){
+	System.out.println(+i+ "*" +n+ "="+i*n);
+}
+	}
+
+}
