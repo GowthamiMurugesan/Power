65e672e5e28d0f05d8e910207749f20f91f8b7b1
GowthamiMurugesan <gowthamimurugesan333@gmail.com>
2016-08-08 11:36:39 +0530

    Add files via upload
    odd

---
+++ Odd.java
@@ -0,0 +1,19 @@
+package guvi;
+
+import java.util.Scanner;
+
+public class Odd {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+		int a,b;
+		a=in.nextInt();
+		b=in.nextInt();
+		for(int i=a;i<=b;i++){
+			if(!(i%2==0)){
+				System.out.println(i);
+			}
+		}
+	}
+
+}
